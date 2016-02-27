1447ffef29cd61fe24317a3082441685a0b771b9
Alexander Sadovsky <alino.sadovsky@gmail.com>
2016-02-27 18:03:37 +0100

    Update build_it.sh

--- tests/build_it.sh
+++ tests/build_it.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t meteorhacks/meteord:base ../base
-docker build -t meteorhacks/meteord:onbuild ../onbuild
-docker build -t meteorhacks/meteord:devbuild ../devbuild
-docker build -t meteorhacks/meteord:binbuild ../binbuild
+docker build -t alino/meteord:base ../base
+docker build -t alino/meteord:onbuild ../onbuild
+docker build -t alino/meteord:devbuild ../devbuild
+docker build -t alino/meteord:binbuild ../binbuild
